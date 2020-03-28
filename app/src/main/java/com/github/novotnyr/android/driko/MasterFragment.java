87ad04f0f5339862e7e655f8b4085c96ae12c1dd
Robert Novotny <novotnr0@gmail.com>
2020-03-28 13:02:26 +0100

    Pridany zoznam ListView s pevnym zoznamom ako resource

--- app/src/main/java/com/github/novotnyr/android/driko/MasterFragment.java
+++ app/src/main/java/com/github/novotnyr/android/driko/MasterFragment.java
@@ -2,10 +2,13 @@ package com.github.novotnyr.android.driko;
 
 import android.os.Bundle;
 import android.view.*;
+import android.widget.ListView;
 
+import androidx.annotation.*;
 import androidx.fragment.app.Fragment;
 
 public class MasterFragment extends Fragment {
+    ListView cityListView;
 
     public MasterFragment() {
         // Required empty public constructor
@@ -15,5 +18,10 @@ public class MasterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_master, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        cityListView = view.findViewById(R.id.cityListView);
     }
 }
