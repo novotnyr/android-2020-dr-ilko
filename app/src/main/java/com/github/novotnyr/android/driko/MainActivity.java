8876b6cef80ea4d544a6ed1c6bda1f6dbe140def
Robert Novotny <novotnr0@gmail.com>
2020-03-28 15:44:44 +0100

    Posluchac v hlavnej aktivite zobrazi detail pri zmene mesta

--- app/src/main/java/com/github/novotnyr/android/driko/MainActivity.java
+++ app/src/main/java/com/github/novotnyr/android/driko/MainActivity.java
@@ -1,6 +1,7 @@
 package com.github.novotnyr.android.driko;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
@@ -14,6 +15,18 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.activity_main, new MasterFragment())
+                .commit();
+
+        ViewModelProvider vmProvider = new ViewModelProvider(this);
+        CityViewModel cityViewModel = vmProvider.get(CityViewModel.class);
+        cityViewModel.getSelectedCity().observe(this, this::showDetailFragment);
+    }
+
+    private void showDetailFragment(String city) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.activity_main, new DetailFragment())
+                .addToBackStack(null)
                 .commit();
     }
 }
